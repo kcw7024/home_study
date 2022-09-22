6ea29016a2a2431b2da5b2df4966c7226d5dc1ef
kcw7024 <107664300+kcw7024@users.noreply.github.com>
2022-09-22 09:41:53 +0900

    commit

--- Yolo/test.py
+++ Yolo/test.py
@@ -4,14 +4,13 @@ import torch
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-img = 'D:\home_study\Yolo/test3.jpg'
+img = 'D:\home_study\Yolo/test.jpg'
 
 results = model(img)
 results.print()
 '''
 image 1/1: 480x640 1 cup, 6 bowls, 2 broccolis, 1 pizza, 1 cake, 1 dining table
 Speed: 47.0ms pre-process, 228.0ms inference, 3.0ms NMS per image at shape (1, 3, 480, 640)
-
 '''
 results.xyxy[0]
 results.show()
