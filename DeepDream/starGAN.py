7f9773c1e1cf1328c07fc49250014f18d900cc07
kcw7024 <107664300+kcw7024@users.noreply.github.com>
2022-09-25 22:50:15 +0900

    commit

---
+++ DeepDream/starGAN.py
@@ -0,0 +1,148 @@
+from types import SimpleNamespace
+
+args = SimpleNamespace()
+
+# Training configuration.
+args.dataset = "CelebA"
+args.selected_attrs = ["Black_Hair", "Blond_Hair", "Brown_Hair", "Male", "Young"]
+args.g_lr = 0.0001
+args.d_lr = 0.0001
+args.beta1 = 0.5
+args.beta2 = 0.999
+
+# Test configuration.
+args.test_iters = 200000
+
+# Model configurations.
+args.c_dim = 5
+args.c2_dim = 8
+args.image_size = 256
+args.g_conv_dim = 64
+args.d_conv_dim = 64
+args.g_repeat_num = 6
+args.d_repeat_num = 6
+
+# Directories.
+args.model_save_dir = "D:\home_study\StarGAN\stargan_celeba_256\models"
+
+# Miscellaneous.
+args.num_workers = 1
+args.mode = "test"
+
+print(args)
+
+
+from model import Generator
+from model import Discriminator
+from torch.autograd import Variable
+from torchvision.utils import save_image
+import torch
+import torch.nn.functional as F
+import numpy as np
+import os
+import time
+import datetime
+
+
+# Solver 클래스에서 실제로 테스트(Test) 목적으로 필요한 함수만 남기고 재구성
+class Solver(object):
+    """Solver for training and testing StarGAN."""
+
+    def __init__(self, config):
+        """Initialize configurations."""
+
+        # Model configurations.
+        self.c_dim = config.c_dim
+        self.c2_dim = config.c2_dim
+        self.image_size = config.image_size
+        self.g_conv_dim = config.g_conv_dim
+        self.d_conv_dim = config.d_conv_dim
+        self.g_repeat_num = config.g_repeat_num
+        self.d_repeat_num = config.d_repeat_num
+
+        # Training configurations.
+        self.dataset = config.dataset
+        self.g_lr = config.g_lr
+        self.d_lr = config.d_lr
+        self.beta1 = config.beta1
+        self.beta2 = config.beta2
+        self.selected_attrs = config.selected_attrs
+
+        # Test configurations.
+        self.test_iters = config.test_iters
+
+        # Miscellaneous.
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        # Directories.
+        self.model_save_dir = config.model_save_dir
+
+        # Build the model and tensorboard.
+        self.build_model()
+
+    def print_network(self, model, name):
+        """Print out the network information."""
+        num_params = 0
+        for p in model.parameters():
+            num_params += p.numel()
+        print(model)
+        print(name)
+        print("The number of parameters: {}".format(num_params))
+
+    def build_model(self):
+        """Create a generator and a discriminator."""
+        if self.dataset in ['CelebA', 'RaFD']:
+            self.G = Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num)
+            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim, self.d_repeat_num) 
+        elif self.dataset in ['Both']:
+            self.G = Generator(self.g_conv_dim, self.c_dim+self.c2_dim+2, self.g_repeat_num)   # 2 for mask vector.
+            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim+self.c2_dim, self.d_repeat_num)
+
+        self.g_optimizer = torch.optim.Adam(self.G.parameters(), self.g_lr, [self.beta1, self.beta2])
+        self.d_optimizer = torch.optim.Adam(self.D.parameters(), self.d_lr, [self.beta1, self.beta2])
+        self.print_network(self.G, 'G')
+        self.print_network(self.D, 'D')
+            
+        self.G.to(self.device)
+        self.D.to(self.device)
+
+    def restore_model(self, resume_iters):
+        """Restore the trained generator and discriminator."""
+        print('Loading the trained models from step {}...'.format(resume_iters))
+        G_path = os.path.join(self.model_save_dir, '{}-G.ckpt'.format(resume_iters))
+        D_path = os.path.join(self.model_save_dir, '{}-D.ckpt'.format(resume_iters))
+        self.G.load_state_dict(torch.load(G_path, map_location=lambda storage, loc: storage))
+        self.D.load_state_dict(torch.load(D_path, map_location=lambda storage, loc: storage))
+
+    def denorm(self, x):
+        """Convert the range from [-1, 1] to [0, 1]."""
+        out = (x + 1) / 2
+        return out.clamp_(0, 1)
+
+    def create_labels(self, c_org, c_dim=5, dataset='CelebA', selected_attrs=None):
+        """Generate target domain labels for debugging and testing."""
+        # Get hair color indices.
+        if dataset == 'CelebA':
+            hair_color_indices = []
+            for i, attr_name in enumerate(selected_attrs):
+                if attr_name in ['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Gray_Hair']:
+                    hair_color_indices.append(i)
+
+        c_trg_list = []
+        for i in range(c_dim):
+            if dataset == 'CelebA':
+                c_trg = c_org.clone()
+                if i in hair_color_indices:  # Set one hair color to 1 and the rest to 0.
+                    c_trg[:, i] = 1
+                    for j in hair_color_indices:
+                        if j != i:
+                            c_trg[:, j] = 0
+                else:
+                    c_trg[:, i] = (c_trg[:, i] == 0)  # Reverse attribute value.
+            elif dataset == 'RaFD':
+                c_trg = self.label2onehot(torch.ones(c_org.size(0))*i, c_dim)
+
+            c_trg_list.append(c_trg.to(self.device))
+        return c_trg_list
+    
+    
